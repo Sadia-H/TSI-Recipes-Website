8f9e0d5a7a10a59874dd930102c03534b7b958c0
Sadia-H <sadiahussain118@gmail.com>
2024-07-23 15:27:07 +0100

    Attempted to add title validation

--- JS/formScript.js
+++ JS/formScript.js
@@ -2,5 +2,19 @@ function formValidation(event) {
     const title = document.getElementById('recipeTitle').value;
     const ingredients = document.getElementById('recipeIngredients').value;
     const recipeInstructions = document.getElementById('recipeInstructions').value;
-    const difficulty = document.querySelector(input[])
+    const difficulty = document.querySelector('input[name="difficulty"]: checked');
+
+    let validDetails = true;
+
+    if (title === "") {
+        displayError('titleError', "Please enter a title.");
+        validDetails = false;
+    }
+
 }
+
+function displayError(id, message) {
+    const errorMessage = document.getElementById(id);
+    errorMessage.textContent = message;
+}
+
