683d79e04513bc3e4dc47208c22d0a058aac6016
Sadia-H <sadiahussain118@gmail.com>
2024-07-23 15:00:10 +0100

    Created script file for form validation

---
+++ JS/formScript.js
@@ -0,0 +1,6 @@
+function formValidation(event) {
+    const title = document.getElementById('recipeTitle').value;
+    const ingredients = document.getElementById('recipeIngredients').value;
+    const recipeInstructions = document.getElementById('recipeInstructions').value;
+    const difficulty = document.querySelector(input[])
+}
