5ca86cf888e78346220a991341bca4ecb94534c3
Sadia-H <sadiahussain118@gmail.com>
2024-07-24 09:44:12 +0100

    Added addIngredient button

--- JS/formScript.js
+++ JS/formScript.js
@@ -1,20 +1,70 @@
-function formValidation(event) {
+
+function validateForm(event) {
+    event.preventDefault();
+
     const title = document.getElementById('recipeTitle').value;
     const ingredients = document.getElementById('recipeIngredients').value;
     const recipeInstructions = document.getElementById('recipeInstructions').value;
-    const difficulty = document.querySelector('input[name="difficulty"]: checked');
-
-    let validDetails = true;
+    const difficulty = document.querySelector('input[name="difficulty"]:checked');
 
     if (title === "") {
-        displayError('titleError', "Please enter a title.");
-        validDetails = false;
+        alert("Please enter a title.");
+        return;
     }
 
+    let ingredientsValid = false;
+    for(let i=0; i<ingredients.length;i++){
+        if(ingredients[i].value !== "") {
+            ingredientsValid = true;
+            break;
+        }
+    }
+
+    if (!ingredientsValid) {
+        alert("Please enter the ingredients");
+        return;
+    }
+    if (recipeInstructions === "") {
+        alert("Please enter the instructions.");
+        return;
+    }
+    if (!difficulty) {
+        alert("Please select a difficulty.");
+        return;
+    }
+
+    document.getElementById('myForm').submit();
 }
 
-function displayError(id, message) {
-    const errorMessage = document.getElementById(id);
-    errorMessage.textContent = message;
+    // document.getElementById('myForm').addEventListener('submit', validateForm);
+   
+    // window.onload = function() {
+    //     document.getElementById('myForm').addEventListener('submit', validateForm);
+    //     // document.getElementById('addIngredient').addEventListener('click', addNewIngredient);
+    // }
+
+function addNewIngredient() {
+    const form = document.getElementById('myForm');
+
+    const newIngredient = document.createElement('div');
+    newIngredient.classList.add('fullInput');
+
+    const newQuantityInput = document.createElement('input');
+    newQuantityInput.type = 'text';
+    newQuantityInput.name = 'quantityIngredients';
+ 
+    const newIngredientInput = document.createElement('input');
+    newIngredientInput.type = 'text';
+    newIngredientInput.name = 'recipeIngredients';
+
+    newIngredient.appendChild(newQuantityInput);
+    newIngredient.appendChild(newIngredientInput);
+
+    form.insertBefore(newIngredient, document.getElementById('addingredient'));
+}
+
+window.onload = function() {
+   document.getElementById('myForm').addEventListener('submit', validateForm);
+   document.getElementById('addIngredient').addEventListener('click', addNewIngredient);
 }
 
